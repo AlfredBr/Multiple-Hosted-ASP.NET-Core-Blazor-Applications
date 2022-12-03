cab840602332256e66f01143efbbb076ff4114a2
Alfred Broderick <AlfredBr@outlook.com>
2022-12-03 14:41:29 -0500

    cleanup

--- host/Pages/Index.cshtml.cs
+++ host/Pages/Index.cshtml.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace host.Pages;
 
-    public class IndexModel : PageModel
+public class IndexModel : PageModel
+{
+    public string Hostname { get; set; } = string.Empty;
+    public void OnGet()
     {
-		public string Hostname { get; set; } = string.Empty;
-        public void OnGet()
-        {
-			Hostname = System.Environment.MachineName;
-        }
+        Hostname = System.Environment.MachineName;
     }
+}
