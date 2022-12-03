b18d7dcb3f03ad8829c202fa232b123e1d8b4d85
Alfred Broderick <alfredbr@gmail.com>
2022-12-03 21:22:51 +0100

    ASPNETCORE_HOSTNAME

--- docker-run.sh
+++ docker-run.sh
@@ -8,5 +8,6 @@ docker run \
 	-p 7003:7003 \
 	-p 7004:7004 \
 	-e ASPNETCORE_ENVIRONMENT="Development" \
+	-e ASPNETCORE_HOSTNAME="funkyhost" \
 	-e ASPNETCORE_URLS="http://*:7000;http://*:7001;http://*:7002;http://*:7003;http://*:7004" \
 	local/hosted-blazor-app
